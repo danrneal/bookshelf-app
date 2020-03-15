b00e0758a38aa523452ddccc0533cff06c26b300
Daniel R. Neal <dan.r.neal@gmail.com>
2020-03-15 12:54:35 -0700

    style: Update docstrings and indentation in models.py

--- backend/models.py
+++ backend/models.py
@@ -8,49 +8,66 @@ database_path = "postgres://{}/{}".format('localhost:5432', database_name)
 
 db = SQLAlchemy()
 
-'''
-setup_db(app)
-    binds a flask application and a SQLAlchemy service
-'''
+
 def setup_db(app, database_path=database_path):
+    """Binds a flask application and SQLAlchemy service
+
+    Args:
+        app: A flask app
+        database_path: A str representing the location of the db
+    """
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
     db.create_all()
 
-'''
-Movie
 
-'''
-class Book(db.Model):  
-  __tablename__ = 'books'
+class Book(db.Model):
+    """A model representing a Book
 
-  id = Column(Integer, primary_key=True)
-  title = Column(String)
-  author = Column(String)
-  rating = Column(Integer)
+    Attributes:
+        id: An int that serves as the unique identifier for a book
+        title: A str representing the title of a book
+        author: A str representing the author of a book
+        rating: An int representing the rating of a book
+    """
 
-  def __init__(self, title, author, rating):
-    self.title = title
-    self.author = author
-    self.rating = rating
+    __tablename__ = 'books'
 
-  def insert(self):
-    db.session.add(self)
-    db.session.commit()
-  
-  def update(self):
-    db.session.commit()
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    author = Column(String)
+    rating = Column(Integer)
 
-  def delete(self):
-    db.session.delete(self)
-    db.session.commit()
+    def __init__(self, title, author, rating):
+        self.title = title
+        self.author = author
+        self.rating = rating
 
-  def format(self):
-    return {
-      'id': self.id,
-      'title': self.title,
-      'author': self.author,
-      'rating': self.rating,
-    }
+    def insert(self):
+        """Inserts a new book object into the db"""
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        """Updates an existing book object in the db"""
+        db.session.commit()
+
+    def delete(self):
+        """Deletes an existing book object from the db"""
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        """Formats the book object as a dict
+
+        Returns:
+            plant: A dict representing the book object
+        """
+        return {
+            'id': self.id,
+            'title': self.title,
+            'author': self.author,
+            'rating': self.rating,
+        }
