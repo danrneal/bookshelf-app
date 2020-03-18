1c0c0a6514b7e1a266b2e226f1bfabaa8cf55ceb
Daniel R. Neal <dan.r.neal@gmail.com>
2020-03-17 17:15:57 -0700

    fix: Use test db rather than prod

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -20,7 +20,7 @@ class BookTestCase(unittest.TestCase):
     def setUp(self):
         self.app = app
         self.client = self.app.test_client
-        self.db_name = 'bookshelf'
+        self.db_name = 'bookshelf_test'
         self.db_path = f'{DB_DIALECT}://{DB_HOST}:{DB_PORT}/{self.db_name}'
         setup_db(self.app, self.db_path)
 
